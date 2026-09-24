209f6e9808d3afd023fec4f08cb2d9745dadb19c
abhishek9874-ctrl <sanjeetsinghrbs@gmail.com>
2026-09-24 23:05:23 +0530

    Learn CRUD Operations

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule, ObserveInstrument } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist:true,
+      forbidNonWhitelisted:true,
+      transform:true,
+      disableErrorMessages:false,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import type {Post as PostInterface } from './interfaces/post.interface'
+import { CreatePostDto } from './dto/create-post.dto';
+import { PostExistsPipe } from './pipes/posts-exists';
 
 @Controller('posts')
 export class PostsController {
@@ -18,31 +20,29 @@ export class PostsController {
         return extractAllPosts;
     }
 
-
     @Get(':id')
-    findOne(@Param('id',ParseIntPipe)id:number):PostInterface{
+    findOne(@Param('id',ParseIntPipe,PostExistsPipe)id:number):PostInterface{
         return this.postsservice.findOne(id);
     }
 
     @Post()
     @HttpCode(HttpStatus.CREATED)
     create(
-        @Body() createPostData:Omit<PostInterface,'id'|'createdAt'>,
-    ):PostInterface{
+        @Body() createPostData:CreatePostDto):PostInterface{
         return this.postsservice.create(createPostData);
     }
 
     @Put(':id')
     update(
-        @Param("id",ParseIntPipe) id:number,
-        @Body() updatePostData:Partial<Omit<PostInterface,"id" | "createdAt">>,
+        @Param("id",ParseIntPipe,PostExistsPipe) id:number,
+        @Body() updatePostData:CreatePostDto
     ): PostInterface{
         return this.postsservice.update(id,updatePostData);
     }
 
     @Delete(':id')
     @HttpCode(HttpStatus.NO_CONTENT)
-    remove(@Param('id',ParseIntPipe) id:number):void{
+    remove(@Param('id',ParseIntPipe,PostExistsPipe) id:number):void{
         this.postsservice.remove(id);
     }
 }
